162d339d140f280607aaee328597bcf05ffee2ce
parkjongjoon7128 <whdwnsdk8111@naver.com>
2024-09-21 03:30:55 +0900

    NaverMap 라이브러리 환경설정 작업 완료

--- src/screens/LocationMapScreen.tsx
+++ src/screens/LocationMapScreen.tsx
@@ -1,16 +1,26 @@
+import { NaverMapView, Region } from '@mj-studio/react-native-naver-map';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
 const LocationMapScreen = () => {
+  // Logic
+  const jejuRegion: Region = {
+    latitude: 33.20530773,
+    longitude: 126.14656715029,
+    latitudeDelta: 0.38,
+    longitudeDelta: 0.8,
+  };
+
+  // View
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        // justifyContent: 'center',
+        // alignItems: 'center',
         backgroundColor: 'white',
       }}>
-      <Text>Location Map Screen</Text>
+      <NaverMapView style={{flex: 1}} locale="ko" initialRegion={jejuRegion} />
     </View>
   );
 }

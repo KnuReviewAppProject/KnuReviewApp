60fb485dc51448acb05fa4daea36e17d22b129b4
parkjongjoon7128 <whdwnsdk8111@naver.com>
2024-09-23 12:14:47 +0900

    클러스터링 기능 구현중

--- src/screens/LocationMapScreen.tsx
+++ src/screens/LocationMapScreen.tsx
@@ -1,20 +1,90 @@
-import { NaverMapMarkerOverlay, NaverMapView, NaverMapViewRef, Region } from '@mj-studio/react-native-naver-map';
-import React, { useEffect, useRef, useState } from 'react';
+import {
+  Camera,
+  ClusterMarkerProp,
+  MapType,
+  NaverMapMarkerOverlay,
+  NaverMapView,
+  NaverMapViewRef,
+  Region,
+} from '@mj-studio/react-native-naver-map';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { View } from 'react-native';
 import { getRestaurants } from '../utils/API/LocationAPI';
 import { Restaurant } from '../utils/data/type';
 
-const LocationMapScreen = () => {
-  // Logic
-  const knuRegion: Region = {
-    latitude: 37.2724406,
-    longitude: 127.1275742,
+const Cameras = {
+  KNU: {
+    latitude: 37.271855,
+    longitude: 127.127626,
+    zoom: 14,
+  },
+} satisfies Record<string, Camera>;
+
+const Regions = {
+  KNU_Region: {
+    latitude: 37.271855,
+    longitude: 127.127626,
     latitudeDelta: 0.01,
     longitudeDelta: 0.01,
-  };
+  },
+} satisfies Record<string, Region>;
 
-  const [restaurants, setRestaurants] = useState<Restaurant[]>([]); // 마커에 사용할 레스토랑 데이터 저장
-  const NaverMapViewRef = useRef<NaverMapViewRef>(null);
+const MapTypes = [
+  'Basic',
+  'Navi',
+  'Satellite',
+  'Hybrid',
+  'Terrain',
+  'NaviHybrid',
+  'None',
+] satisfies MapType[];
+
+const LocationMapScreen = () => {
+  // Logic
+  const ref = useRef<NaverMapViewRef>(null);
+  const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+  const [indoor, setIndoor] = useState(false);
+  const [mapType, setMapType] = useState<MapType>(MapTypes[0]!);
+  const [lightness, setLightness] = useState(0);
+  const [compass, setCompass] = useState(true);
+  const [scaleBar, setScaleBar] = useState(true);
+  const [zoomControls, setZoomControls] = useState(true);
+  const [indoorLevelPicker, setIndoorLevelPicker] = useState(true);
+  const [myLocation, setMyLocation] = useState(true);
+  const [hash, setHash] = useState(0);
+  const [camera, setCamera] = useState(Cameras.KNU);
+
+  const clusters = useMemo<
+    {
+      markers: ClusterMarkerProp[];
+      screenDistance?: number;
+      minZoom?: number;
+      maxZoom?: number;
+      animate?: boolean;
+      width?: number;
+      height?: number;
+    }[]
+  >(() => {
+    return restaurants.map(i => {
+      return {
+        width: 200,
+        height: 200,
+        markers: restaurants.map<ClusterMarkerProp>(
+          j =>
+            ({
+              image: {
+                httpUri: `https://picsum.photos/seed/${hash}-${i}-${j}/32/32`,
+              },
+              width: 100,
+              height: 100,
+              latitude: Cameras.KNU.latitude + Math.random() + 1.5,
+              longitude: Cameras.KNU.longitude + Math.random() + 1.5,
+              identifier: `${hash}-${i}-${j}`,
+            } satisfies ClusterMarkerProp),
+        ),
+      };
+    });
+  }, [hash]);
 
   useEffect(() => {
     getRestaurants(setRestaurants);
@@ -29,23 +99,43 @@ const LocationMapScreen = () => {
       }}>
       <NaverMapView
         style={{flex: 1}}
-        ref={NaverMapViewRef}
+        ref={ref}
         initialCamera={{
-          latitude: 37.2718550, 
-          longitude: 127.1276260,
-          zoom: 15
+          latitude: 37.271855,
+          longitude: 127.127626,
+          zoom: 15,
+        }}
+        locale="ko"
+        layerGroups={{
+          BUILDING: true,
+          BICYCLE: false,
+          CADASTRAL: false,
+          MOUNTAIN: false,
+          TRAFFIC: false,
+          TRANSIT: false,
+        }}
+        mapType={mapType}
+        initialRegion={Regions.KNU_Region}
+        camera={camera}
+        isIndoorEnabled={indoor}
+        isShowCompass={compass}
+        isShowIndoorLevelPicker={indoorLevelPicker}
+        isShowScaleBar={scaleBar}
+        isShowZoomControls={zoomControls}
+        isShowLocationButton={myLocation}
+        lightness={lightness}
+        clusters={clusters}
+        onInitialized={() => console.log('initialized!')}
+        onTapClusterLeaf={({markerIdentifier}) => {
+          console.log('onTapClusterLeaf', markerIdentifier);
         }}>
         {restaurants.map((restaurant, index) => (
           <NaverMapMarkerOverlay
-            key={index}
+            key={restaurant.id}
             latitude={parseFloat(restaurant.y)}
             longitude={parseFloat(restaurant.x)}
             onTap={() => console.log(`Tapped on: ${restaurant.name}`)}
             anchor={{x: 0.5, y: 1}}
-            caption={{
-              key: `${restaurant.id}`,
-              text: restaurant.name, // 레스토랑 이름 표시
-            }}
             width={20}
             height={20}>
             <View style={{width: 50, height: 50, backgroundColor: 'red'}} />
